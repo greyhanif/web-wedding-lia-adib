90a061d03b8f64e52757964470e874aad1f7d44e
greyhanif <grey.hanif@gmail.com>
2021-12-24 04:26:22 +0700

    mengubah server index

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
-// const express = require("express");
-// const path = require("path");
-// const fs = require("fs");
-import express from "express";
-import path from "path";
-import fs from "fs";
-import axios from "axios";
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const axios = require("axios");
+// import express from "express";
+// import path from "path";
+// import fs from "fs";
+// import axios from "axios";
 
 // const { getPostById } = require("./stub/posts");
 const app = express();
@@ -14,46 +15,38 @@ const indexPath = path.resolve(__dirname, "..", "build", "index.html");
 
 // static resources should just be served as they are
 app.use(express.static(path.resolve(__dirname, "..", "build"), { maxAge: "30d" }));
-// here we serve the index.html page
-app.get("/", (req, res, next) => {
-  fs.readFile(indexPath, "utf8", (err, htmlData) => {
-    if (err) {
-      console.error("Error during file reading", err);
-      return res.status(404).end();
-    }
-    // inject meta tags
-    htmlData = htmlData
-      .replace("<title>Undangan Pernikahan Lia & Adib</title>", `<title>${post.title}</title>`)
-      .replace("__META_OG_TITLE__", `Undangan Pernikahan Lia & Adib`)
-      .replace("__META_OG_DESCRIPTION__", `Kepada Bapak/Ibu/Sodara/i di tempat`)
-      .replace("__META_DESCRIPTION__", `Kepada Bapak/Ibu/Sodara/i di tempat`)
-      .replace("__META_OG_IMAGE__", `src`);
-    return res.send(htmlData);
-  });
-});
 
-// here we serve the index.html page
 app.get("/rsvp/to/:slug", (req, res, next) => {
-  fs.readFile(indexPath, "utf8", (err, htmlData) => {
+  fs.readFile(indexPath, "utf8", async (err, htmlData) => {
     if (err) {
       console.error("Error during file reading", err);
       return res.status(404).end();
     }
     // get post info
+    // const contacts = axios.post(`https://api.liaadib-weddingday.id/apps/` + slug);
     const slug = req.params.slug;
-    const contacts = axios.post(`https://api.liaadib-weddingday.id/apps/` + slug);
-    if (!contacts) return res.status(404).send("Post not found");
+    const contacts = await axios.get(`http://localhost:5000/apps/` + slug);
+    console.log(contacts.data);
+    console.log(slug);
+    if (contacts.data.msg === "no data available") {
+      return res.status(404).send("Post not found");
+    } else {
+      let name = contacts.data.contacts[0].name;
+      let city = contacts.data.contacts[0].city;
+      let ticketCode = contacts.data.tickets[0].ticketCode;
 
-    // inject meta tags
-    htmlData = htmlData
-      .replace("<title>Undangan Pernikahan Lia & Adib</title>", `<title>${post.title}</title>`)
-      .replace("__META_OG_TITLE__", post.title)
-      .replace("__META_OG_DESCRIPTION__", post.description)
-      .replace("__META_DESCRIPTION__", post.description)
-      .replace("__META_OG_IMAGE__", post.thumbnail);
-    return res.send(htmlData);
+      // inject meta tags
+      htmlData = htmlData
+        .replace("<title>Undangan Pernikahan Lia & Adib</title>", `<title>Kepada ${name}</title>`)
+        .replace("__META_OG_TITLE__", `Kepada ${name}`)
+        .replace("__META_OG_DESCRIPTION__", `Kepada ${name}`)
+        .replace("__META_DESCRIPTION__", `Kepada ${name}`)
+        .replace("__META_OG_IMAGE__", `Kepada ${name}`);
+      return res.send(htmlData);
+    }
   });
 });
+
 // listening...
 app.listen(PORT, (error) => {
   if (error) {
