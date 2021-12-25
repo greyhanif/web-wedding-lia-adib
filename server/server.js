25799f5c78df37a9fc9f6420ada83f1090261fc9
greyhanif <grey.hanif@gmail.com>
2021-12-25 13:40:04 +0700

    res to response @ server

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ app.get("/rsvp/to/:slug", (request, response) => {
     // console.log(contacts.data);
 
     if (contacts.data.msg === "no data available") {
-      return res.status(404).send("Post not found");
+      return response.status(404).send("Post not found");
     } else {
       let name = contacts.data.contacts[0].name;
       let city = contacts.data.contacts[0].city;
